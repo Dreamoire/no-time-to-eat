a20a3fd323c6351c7b19a1792bf9b3115e3b7c26
abijoserah <serahabijo94@gmail.com>
2025-11-12 22:37:21 +0100

    Ajout d'un état type de recherche qui permet de passer de recipe à ingredients en cliquant sur le switch

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,15 +2,42 @@ import { useState } from "react";
 import "../styles/SearchBar.css";
 
 export function SearchBar() {
+	const [searchType, setSearchType] = useState<"recipe" | "ingredient">(
+		"recipe",
+	);
 	return (
 		<div className="search">
-			<div className="search-switch">
-				<span>Recipe</span>
-				<input type="checkbox" id="switch" name="switch" />
+			<div
+				className="search-switch"
+				style={{
+					backgroundColor:
+						searchType === "recipe"
+							? "var(--bg-color-brightred)"
+							: "var(--bg-color-darkred)",
+				}}
+			>
+				<span>{searchType === "recipe" ? "Recipe" : "Ingredient"}</span>
+				<input
+					type="checkbox"
+					checked={searchType === "ingredient"}
+					id="switch"
+					name="switch"
+					onClick={() =>
+						setSearchType(searchType !== "recipe" ? "recipe" : "ingredient")
+					}
+				/>
 				<label htmlFor="switch">""</label>
 			</div>
+
 			<div className="search-bar">
-				<input placeholder="Search a recipe" className="search-input" />
+				<input
+					placeholder={
+						searchType === "recipe"
+							? "Search a recipe"
+							: "Enter your ingredients"
+					}
+					className="search-input"
+				/>
 				<button type="button" className="search-btn">
 					<img src="src/assets/images/search.png" alt="Search icon" />
 				</button>
