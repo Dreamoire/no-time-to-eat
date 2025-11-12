46e77bf43c6e2a214a90a9de6a76e4dbcd9d224b
abijoserah <serahabijo94@gmail.com>
2025-11-12 21:41:12 +0100

    Correction recette en recipe sur le switch

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,13 @@
+import { useState } from "react";
 import "../styles/SearchBar.css";
 
 export function SearchBar() {
 	return (
 		<div className="search">
 			<div className="search-switch">
-				<span>Recette</span>
+				<span>Recipe</span>
 				<input type="checkbox" id="switch" name="switch" />
-				<label htmlFor="switch">Recette</label>
+				<label htmlFor="switch">""</label>
 			</div>
 			<div className="search-bar">
 				<input placeholder="Search a recipe" className="search-input" />
