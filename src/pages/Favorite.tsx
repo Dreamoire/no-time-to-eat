cdbe21b8aff3b07ed67295b7a2970a925780ee2a
FIRST_NAME LAST_NAME <julpaultes@gmail.com>
2025-11-17 12:16:56 +0100

    création du composant Favorite

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -1,3 +1,35 @@
+import { useOutletContext } from "react-router";
+import RecipeCard from "../components/RecipeCard";
+import type { RecipeType } from "../types/recipe";
+
+export interface Context {
+	favoriteIds: string[];
+	toggleFavorite: (idMeal: string) => void;
+	recipes: RecipeType[];
+	getRecipes: () => void;
+}
+
 export default function Favorite() {
-	return <h1>Favorite</h1>;
+	const { favoriteIds, toggleFavorite, recipes } = useOutletContext<Context>();
+
+	const favoritesRecipes = recipes.filter((recipe) =>
+		favoriteIds.includes(recipe.idMeal),
+	);
+	return (
+		<>
+			<section>
+				{favoritesRecipes.map((recipe) => {
+					const isFavorite = favoriteIds.includes(recipe.idMeal);
+					return (
+						<RecipeCard
+							recipe={recipe}
+							isFavorite={isFavorite}
+							onToggleFavorite={toggleFavorite}
+							key={recipe.idMeal}
+						/>
+					);
+				})}
+			</section>
+		</>
+	);
 }
