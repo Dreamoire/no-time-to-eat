55ec66f96bcd60d955e930c9efc2949f348a3279
Yuliana Krasulya <krasulyayuliana@gmail.com>
2025-11-25 12:52:55 +0100

    Correction finale App.tsx avant changement de branche

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet, useLocation } from "react-router";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
 
 import "./styles/reset.css";
 import "./index.css";
