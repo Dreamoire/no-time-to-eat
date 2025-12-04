f41874ffbc7a87ac8760170a1f781378c5159b79
Yuliana Krasulya <krasulyayuliana@gmail.com>
2025-12-04 16:26:54 +0100

    fix: set Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/no-time-to-eat/",      // ← добавили ВАЖНУЮ строку
   plugins: [react()],
   // server: {
   //   proxy: {
@@ -12,4 +13,5 @@ export default defineConfig({
   //     }
   //   }
   // }
-})
+});
+
