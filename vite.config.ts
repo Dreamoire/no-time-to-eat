3927be39dac0fcce43e0c0c4065a3a7f10e0149d
Yuliana Krasulya <krasulyayuliana@gmail.com>
2025-12-04 21:07:45 +0100

    Correction de style pour les écrans mobiles, desing responsif

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/no-time-to-eat/",      // ← добавили ВАЖНУЮ строку
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://urlofyourapiblockedbycors",
-  //       changeOrigin: true
-  //     }
-  //   }
-  // }
 });
-
