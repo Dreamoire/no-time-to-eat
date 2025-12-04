2256e51fd724c167efc96fb6031f15e3359ce0d7
Yuliana Krasulya <krasulyayuliana@gmail.com>
2025-12-04 14:36:19 +0100

    corrections des styles

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,7 +3,7 @@ import "../styles/SignUp.css";
 export default function SignUp() {
 	return (
 		<div className="signup-card">
-			<p className="signup-error">La fameuse erreur 404 !!!</p>
+			<p className="signup-error">Soon avalaible</p>
 			<img
 				alt="formateurs"
 				src="src/assets/images/formateurs.png"
